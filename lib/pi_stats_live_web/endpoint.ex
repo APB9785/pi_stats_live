1e4388c3e1dafb99f8dd0bf2754444552ca7c438
APB9785 <74077809+APB9785@users.noreply.github.com>
2021-04-28 20:05:47 -0500

    Remove Ecto callback from endpoint

--- lib/pi_stats_live_web/endpoint.ex
+++ lib/pi_stats_live_web/endpoint.ex
@@ -32,7 +32,6 @@ defmodule PiStatsLiveWeb.Endpoint do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
-    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :pi_stats_live
   end
 
   plug Phoenix.LiveDashboard.RequestLogger,
