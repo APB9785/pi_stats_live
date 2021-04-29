013a3f335cb2167cb27c63bfb21d130e9f291869
APB9785 <74077809+APB9785@users.noreply.github.com>
2021-04-29 17:16:07 -0500

    Removed unused data from socket assigns

--- lib/pi_stats_live/sys_procs.ex
+++ lib/pi_stats_live/sys_procs.ex
@@ -11,54 +11,51 @@ defmodule PiStatsLive.SysProcs do
     end
   end
 
-  def parse_top([times, tasks, cpu, mem, swap, _headers | rest]) do
-    # Old parser for times, doesn't match all cases
-    #[_, _, current_time, _, days_up, _, hhmm_up, users, _, _, _, load_1m, load_5m, load_15m] =
-    #  String.split(times, " ", trim: true)
-
-    #[hh, mm] = String.slice(hhmm_up, 0..-2) |> String.split(":")
-
-    {current_time, uptime, users, load_1m, load_5m, load_15m} =
+  def parse_top([times, _tasks, cpu, mem, _swap, _headers | processes]) do
+    {current_time, uptime, _users, _load_1m, _load_5m, _load_15m} =
       times
       |> String.split(" ", trim: true)
       |> parse_times
 
-    [_, tasks, _, running, _, sleeping, _, stopped, _, zombie, _] =
-      String.split(tasks, " ", trim: true)
+    # [_, tasks, _, running, _, sleeping, _, stopped, _, zombie, _] =
+    #   String.split(tasks, " ", trim: true)
 
     [_, _us_cpu, _, _sy_cpu, _, _ni_cpu, _, id_cpu, _, _wa_cpu, _, _hi_cpu, _, _si_cpu, _, _st_cpu, _] =
       String.split(cpu, " ", trim: true)
 
-    [_, _, _, total_mem, _, free_mem, _, used_mem, _, buffer_mem, _] =
+    [_, _, _, total_mem, _, _free_mem, _, used_mem, _, _buffer_mem, _] =
       String.split(mem, " ", trim: true)
 
-    [_, _, _, _, free_swap, _, used_swap, _, swap_total, _, _] =
-      String.split(swap, " ", trim: true)
+    # [_, _, _, _, free_swap, _, used_swap, _, swap_total, _, _] =
+    #   String.split(swap, " ", trim: true)
 
     {total_mem, _} = Float.parse(total_mem)
     {used_mem, _} = Float.parse(used_mem)
 
-    %{:current_time => current_time,
-      :uptime => uptime,
-      :user_count => users,
-      :avg_load_1m => String.slice(load_1m, 0..-2),
-      :avg_load_5m => String.slice(load_5m, 0..-2),
-      :avg_load_15m => load_15m,
-      :total_tasks => tasks,
-      :running_tasks => running,
-      :sleeping_tasks => sleeping,
-      :stopped_tasks => stopped,
-      :zombie_tasks => zombie,
-      :cpu_usage => format_cpu(id_cpu),
-      :total_memory => total_mem,
-      :free_memory => free_mem,
-      :used_memory => used_mem,
-      :buffer_memory => buffer_mem,
-      :free_swap_percent => free_swap,
-      :used_swap_percent => used_swap,
-      :swap_total_size => swap_total,
-      :process_list => Enum.map(rest, &parse_process/1)
-    }
+    # Unused assigns
+    # user_count: users,
+    # avg_load_1m: String.slice(load_1m, 0..-2),
+    # avg_load_5m: String.slice(load_5m, 0..-2),
+    # avg_load_15m: load_15m,
+    # total_tasks: tasks,
+    # running_tasks: running,
+    # sleeping_tasks: sleeping,
+    # stopped_tasks: stopped,
+    # zombie_tasks: zombie,
+    # free_memory: free_mem,
+    # buffer_memory: buffer_mem,
+    # free_swap_percent: free_swap,
+    # used_swap_percent: used_swap,
+    # swap_total_size: swap_total,
+
+    [
+      current_time: current_time,
+      uptime: uptime,
+      cpu_usage: format_cpu(id_cpu),
+      total_memory: total_mem,
+      used_memory: used_mem,
+      process_list: Enum.map(processes, &parse_process/1)
+    ]
   end
 
   def format_cpu(id_cpu) do
