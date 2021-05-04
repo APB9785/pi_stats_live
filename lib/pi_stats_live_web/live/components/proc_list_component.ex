18b318830369045a6ca258719c595f032c1bfe1e
APB9785 <74077809+APB9785@users.noreply.github.com>
2021-05-04 12:06:15 -0500

    Move process list into LC

---
+++ lib/pi_stats_live_web/live/components/proc_list_component.ex
@@ -0,0 +1,31 @@
+defmodule PiStatsLiveWeb.ProcListComponent do
+  @moduledoc """
+  Process list
+  """
+
+  use PiStatsLiveWeb, :live_component
+
+  def render(assigns) do
+    ~L"""
+    <div class="bg-white p-8 rounded-xl shadow-md">
+      <h3 align="left" class="pb-4">Active Processes:</h3>
+      <table class="p-0">
+        <tr>
+          <th class="text-sm p-0">Name</th>
+          <th class="text-sm p-0">Uptime</th>
+          <th class="text-sm p-0">CPU Use</th>
+          <th class="text-sm p-0">RAM Use</th>
+        </tr>
+        <%= for proc <- @process_list do %>
+          <tr>
+            <td class="text-sm p-0"><%= proc["name"] %></td>
+            <td class="text-sm p-0"><%= proc["uptime"] %></td>
+            <td class="text-sm p-0"><%= proc["cpu"] %> %</td>
+            <td class="text-sm p-0"><%= proc["mem"] %> %</td>
+          </tr>
+        <% end %>
+      </table>
+    </div>
+    """
+  end
+end
